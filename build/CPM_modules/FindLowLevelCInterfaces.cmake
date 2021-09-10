099344030d1cd5364d137eae96a38dad252da3f3
dnatov <dnatov@gsdx.us>
2021-09-10 12:53:32 +0200

    Add gitignore and clean

--- build/CPM_modules/FindLowLevelCInterfaces.cmake
+++
@@ -1,3 +0,0 @@
-include(C:/GSDSoftware/OtherRepos/SPI-IN-PI42TAS/cpm/CPM.cmake)
-CPMAddPackage(GITHUB_REPOSITORY;JeroenVandezande/LowLevelCInterfaces;VERSION;1.1.0;EXCLUDE_FROM_ALL;YES)
-set(LowLevelCInterfaces_FOUND TRUE)
